4736ded3d9882f707eccf98ae3b2b757359413b6
maliming <malimings@gmail.com>
2018-03-15 17:59:19 +0800

    STEP 1

--- RegionTree/Program.cs
+++ RegionTree/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
 
 namespace RegionTree
 {
@@ -6,6 +8,23 @@ namespace RegionTree
     {
         static void Main(string[] args)
         {
+
+            var client = new HttpClient();
+
+            var regionWebResponseMessage = client.GetAsync("http://www.mca.gov.cn/article/sj/tjbz/a/2018/201803131439.html")
+                .Result;
+
+            var regionResult = regionWebResponseMessage.Content.ReadAsStringAsync().Result;
+
+            var regionCodes =  Regex.Matches(regionResult, @"<td class=xl\d{1,10}>(\d{6})</td>");
+            var regionNames = Regex.Matches(regionResult, @"<td class=xl\d{1,10}>([^0-9a-zA-Z<>]+)<");
+
+            foreach (Match code in regionCodes)
+            {
+                Console.WriteLine(code.Value);
+            }
+
+
             Console.WriteLine("Hello World!");
         }
     }
